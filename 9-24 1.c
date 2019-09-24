2f6061cc93a28ea6320fd0ad7ced459ad173e439
WE-hai <55355504+WE-hai@users.noreply.github.com>
2019-09-24 22:58:28 +0800

    Add files via upload

---
+++ 9-24 1.c
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	int arr[] = {1,2,3,4,5,6,7,8,9,10};
+	int k = 0;
+	scanf("%d", &k);
+	int sz = sizeof(arr)/sizeof(arr[0]);
+	int left = 0;
+	int right = sz-1;
+		while(left<=right)
+	{
+		int mid = (left+right)/2;
+		if(arr[mid] > k)
+		{
+			right = mid-1;
+		}
+		else if(arr[mid] < k)
+		{
+			left = mid+1;
+		}
+		else
+		{
+			printf("找到了，下标是：%d\n", mid);
+			break;
+		}
+	}
+	if(left>right)
+	{
+		printf("很遗憾，找不到\n");
+	}
+	return 0;
+}
