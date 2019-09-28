71879e8b56c9149a6f55e8d4c0f4639f78860b68
WE-hai <55355504+WE-hai@users.noreply.github.com>
2019-09-28 21:58:53 +0800

    Add files via upload

---
+++ P10 9-26/P10 9-26/9-26 1.c
@@ -0,0 +1,22 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	char c;
+	scanf("%c", &c);
+	if (c >= 'A'&&c<='Z')
+	{
+		c = c + 32;
+		printf("%c\n", c);
+	}
+	else if (c>='a'&&c<='z')
+	{
+		c = c - 32;
+		printf("%c\n", c);
+	}
+	else
+	{
+		printf("ÊäÈë´íÎó£¡\n");
+	}
+	return 0;
+}
