59cb91eb02fb0a4eaa479e22ee492557f5b6363e
WE-hai <55355504+WE-hai@users.noreply.github.com>
2019-09-28 22:01:51 +0800

    Add files via upload

---
+++ P9 9-25/P9 9-25/9-25 4.c
@@ -0,0 +1,42 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//int binary_search(int arr[], int k, int sz)
+//{
+//	int left = 0;
+//	int right = sz-1;
+//	while(left<=right)
+//	{
+//		int mid = (left+right)/2;
+//		if(arr[mid] > k)
+//		{
+//			right = mid-1;
+//		}
+//		else if(arr[mid] < k)
+//		{
+//			left = mid+1;
+//		}
+//		else
+//		{
+//			return mid;
+//		}
+//	}
+//	return -1;
+//}
+//int main()
+//{
+//	int arr[] = {1,2,3,4,5,6,7,8,9,10};
+//	int k = 0;
+//	scanf("%d", &k);
+//	int sz = sizeof(arr)/sizeof(arr[0]);
+//	int ret = 0;
+//	ret = binary_search(arr, k, sz);
+//	if(ret == -1)
+//	{
+//		printf("很遗憾，找不到\n");
+//	}
+//	else
+//	{
+//		printf("找到了，下标是：%d\n", ret);
+//	}
+//	return 0;
+//}
