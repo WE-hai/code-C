5674bf14cfab8c3b33be849d6376a6e3bfcdb0df
WE-hai <55355504+WE-hai@users.noreply.github.com>
2019-10-03 22:27:59 +0800

    Add files via upload

---
+++ 10-3 5.c
@@ -0,0 +1,23 @@
+
+
+switch (n)
+{
+case 1:
+{
+		  init(arr, sz);
+		  printf("\n");
+}
+case 2:
+{
+		  empty(arr, sz);
+		  printf("\n");
+}
+case 3:
+{
+		  reverse(arr, sz);
+		  printf("\n");
+}
+default:
+{
+		   printf("´íÎó");
+}
