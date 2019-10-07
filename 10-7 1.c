fec8b7f90bd272840af24403323c1d0626956219
WE-hai <55355504+WE-hai@users.noreply.github.com>
2019-10-07 19:39:39 +0800

    Add files via upload

---
+++ 10-7 1.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+	int i = 100;
+	int count = 0;
+	while (i <= 200)
+	{
+		int flag = 0;
+		int j = 2;
+		while (j<i)
+		{
+			if (i%j == 0)
+			{
+				flag = 1;
+				break;
+			}
+			j++;
+		}
+		if (flag == 0)
+		{
+			count++;
+			printf("%d ", i);
+		}
+		i++;
+	}
+	printf("\ncount = %d\n", count);
+	return 0;
+}
