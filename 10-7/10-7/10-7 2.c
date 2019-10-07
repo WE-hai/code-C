25550fb06917cdcb00dabc7958c495f435533e3b
WE-hai <55355504+WE-hai@users.noreply.github.com>
2019-10-07 20:01:15 +0800

    Add files via upload

---
+++ 10-7/10-7/10-7 2.c
@@ -0,0 +1,17 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+int main()
+{
+	int n = 0;
+	int m = 1;
+	int i = 0;
+	printf("请输入一个数：");
+	scanf("%d", &n);
+	for (i = 1; i <= n; i++)
+	{
+		m *= i;
+	}
+	printf("%d!=%d\n",n, m);
+	return 0;
+}
