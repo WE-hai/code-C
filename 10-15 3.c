259ef038680a331884c14095382e3aba8d5aeb57
WE-hai <943239470@qq.com>
2019-10-15 19:22:21 +0800

    add

---
+++ 10-15 3.c
@@ -0,0 +1,23 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	int arr[10] = { 0 };
+	int i = 0;
+	int max = 0;
+	printf("«Î ‰»Î10∏ˆ ˝£∫");
+	for (i = 0; i < 10; i++)
+	{
+		scanf("%d", arr);
+	}
+	max = arr[0];
+	for (i = 1; i < 10; i++)
+	{
+		if (arr[i]>max)
+		{
+			max = arr[i];
+		}
+	}
+	printf("max=%d\n", max);
+	return 0;
+}
