46ae3b8012097194db300e72f9dd4c99c9d987ad
WE-hai <943239470@qq.com>
2019-10-21 20:54:35 +0800

    add

---
+++ 10-21 2.c
@@ -0,0 +1,12 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	char ch;
+	printf("ÇëÊäÈëÒ»¸ö×Ö·û£º\n");
+	scanf("%c", &ch);
+	ch = (ch >= 'A'&&ch <= 'Z') ? (ch + 32) : ch;
+	ch = (ch >= 'a'&&ch <= 'z') ? (ch - 32) : ch;
+	printf("%c\n", ch);
+	return 0;
+}
