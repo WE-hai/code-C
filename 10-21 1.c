3757ce65b5a571c9845f7f65a23c7e4c6a986e38
WE-hai <943239470@qq.com>
2019-10-21 20:47:06 +0800

    add

---
+++ 10-21 1.c
@@ -0,0 +1,29 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	int arr1[2][3] = { { 1, 2, 3 }, { 4, 5, 6, } };
+	int arr2[3][2] = { 0 };
+	int i = 0;
+	int j = 0;
+	printf("arr1:\n");
+	for (i = 0; i <= 1; i++)
+	{
+		for (j = 0; j <= 2; j++)
+		{
+			printf("%d ", arr1[i][j]);
+			arr2[j][i] = arr1[i][j];
+		}
+		printf("\n");
+	}
+	printf("arr2:\n");
+	for (i = 0; i <= 2; i++)
+	{
+		for (j = 0; j <= 1; j++)
+		{
+			printf("%d ", arr2[i][j]);
+		}
+		printf("\n");
+	}
+	return 0;
+}
